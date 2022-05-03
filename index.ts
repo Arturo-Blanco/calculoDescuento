e789afc31c485d3b956ae58778dd2f4a4e104f02
Jose Mauricio Benitez <52261677+xX-RONIN-Xx@users.noreply.github.com>
2022-05-02 21:52:27 -0300

    initial commit

--- index.ts
+++
@@ -1,9 +0,0 @@
-let precioProducto = document.getElementById("precioProducto");
-let btnPrecio = document.getElementById("btnPrecio");
-
-btnPrecio.addEventListener("click", () => {
-  console.log(
-    "El precio final es ",
-    precioProducto.value - precioProducto.value * 0.1
-  );
-});
